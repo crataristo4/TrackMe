481fd3aa5d727ee3b1578c24585631463f281601
Aristocrat <crataristo4@gmail.com>
2021-07-08 18:36:38 +0000

    added log and sign up

---
+++ app/src/main/java/com/track/me/app/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.track.me.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.track.me.app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
